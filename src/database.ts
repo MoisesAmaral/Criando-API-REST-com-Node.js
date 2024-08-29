0ecbd409b51dc7bb2c833caabe0942d87f21ed6d
MoisesAmaral <moises.amaraldev@gmail.com>
2024-08-29 14:39:49 -0300

    AJUSTE DATABASE_CLIENT

--- src/database.ts
+++ src/database.ts
@@ -2,7 +2,7 @@ import { knex as setupKnex, Knex } from 'knex'
 import { env } from './env'
 
 export const config: Knex.Config = {
-  client: env.DATA_BASE_CLIENT,
+  client: env.DATABASE_CLIENT,
   connection:
     env.DATABASE_URL === 'sqlite'
       ? {
--- src/env/index.ts
+++ src/env/index.ts
@@ -9,7 +9,7 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
-  DATA_BASE_CLIENT: z.enum(['pg', 'sqlite3']),
+  DATABASE_CLIENT: z.enum(['pg', 'sqlite3']),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 })
