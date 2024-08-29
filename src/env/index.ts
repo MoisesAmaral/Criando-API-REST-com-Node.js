35d8166b854779eca14fbfbd65334801ceaa6b48
MoisesAmaral <moises.amaraldev@gmail.com>
2024-08-29 14:26:30 -0300

    config deploy

--- src/env/index.ts
+++ src/env/index.ts
@@ -9,8 +9,9 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATA_BASE_CLIENT: z.enum(['pg', 'sqlite3']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
